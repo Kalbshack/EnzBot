035743045910be227c5dad24bec4c4224082cfc7
Kalbshack <sp.id01@gmx.de>
2015-08-04 13:23:23 +0200

    minimal Image support

---
+++ image.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+
+#Different type/sizes of fonts
+title = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",30)
+semititle = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",25)
+cright = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",10)
+main = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",15)
+
+def makeImage(weather):
+    time = weather[0]
+    wTemp = weather[1]
+    aTemp = weather[2]
+    icon = weather[3]
+    forecast = weather[4]
+    #Get the weather symbol
+    miniIconSize = 25
+    icn = Image.open('icons/' + icon + '.png')
+    icn.thumbnail((80, 80), Image.ANTIALIAS)
+
+    aTempIcon = Image.open('icons/aTemp.png')
+    wTempIcon = Image.open('icons/wTemp.png')
+    wTempIcon.thumbnail((25,25), Image.ANTIALIAS)
+
+    forIcon = []
+    forIcon.append(Image.open('icons/' + forecast[0][2] + '.png'))
+    forIcon[0].thumbnail((miniIconSize, miniIconSize), Image.ANTIALIAS)
+    forIcon.append(Image.open('icons/' + forecast[1][2] + '.png'))
+    forIcon[1].thumbnail((miniIconSize, miniIconSize), Image.ANTIALIAS)
+    forIcon.append(Image.open('icons/' + forecast[2][2] + '.png'))
+    forIcon[2].thumbnail((miniIconSize, miniIconSize), Image.ANTIALIAS)
+
+    white = Image.new("RGBA", (80,80),(255,255,255))
+    forWhite = Image.new("RGBA", (miniIconSize,miniIconSize),(255,255,255))
+    degSign = '°'.decode('utf-8')
+
+    #img = Image.new("RGBA", (300,150),(14,93,138))
+    img = Image.open('bg.jpeg')
+    img.paste(white, (210,5), icn)
+    draw = ImageDraw.Draw(img)
+    draw.text((5, 5),('Enz ' + time),(255,255,255),font=title)
+    draw.text((218, 90),str(aTemp) + degSign,(255,255,255),font=title)
+    img.paste(aTempIcon, (200, 80), aTempIcon)
+    draw.text((230, 120),str(wTemp) + degSign,(255,255,255),font=semititle)
+    img.paste(wTempIcon, (200, 124), wTempIcon)
+    draw.text((5, 65),forecast[0][0] + "          " + str(forecast[0][1]) + degSign,(255,255,255),font=main)
+    img.paste(forWhite, (65,60), forIcon[0])
+    draw.text((5, 95),"21:00          33.0" + degSign,(255,255,255),font=main)
+    img.paste(forWhite, (65,90), forIcon[1])
+    draw.text((5, 125),"24:00          13.7" + degSign,(255,255,255),font=main)
+    img.paste(forWhite, (65,120), forIcon[2])
+    draw = ImageDraw.Draw(img)
+    #img.save("test.png")
+    img.save("/home/lars/web/test.png")
---
+++ weather.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+import urllib2
+import csv
+import json
+import time
+import bottoken
+
+def getWeather():
+    return [getAirCondition()[0], getWaterTemp(0), "%.1f" % getAirCondition()[1], getAirCondition()[2], getForecast()]
+
+def getWaterTempVal():
+    #Url with the table(csv) of temperatures from enz in bessigheim
+    url = "https://www.lubw.baden-wuerttemberg.de/servlet/is/84696/data.csv"
+    try:
+        #Try reading the values from the url
+        response = urllib2.urlopen(url)
+        spamreader = csv.reader(response, delimiter=';', quotechar='|')
+        #Get rid of description from the csv
+        spamreader.next()
+        #Get the actual values
+        tmp = spamreader.next()
+        #Parse the values from the table content
+        time = int(tmp[0].split(' ')[1].split(":")[0]) + 1
+        temp = float(".".join(tmp[1].split(",")))
+
+        return [temp, time]
+    except:
+        return -1
+
+def getAirCondition():
+    try:
+        url = 'http://api.openweathermap.org/data/2.5/weather/?id=2949012&lang=de&units=metric&APPID=' + bottoken.weatherApiKey
+        response = urllib2.urlopen(url);
+        data = json.loads(response.read())
+        wTime = time.strftime("%H:%M", time.gmtime(data.get('dt', '')))
+        wDesc = str(data.get('weather', '')[0].get('description', '').encode('utf-8'))
+        wTemp = data.get('main', '').get('temp', '')
+        wIcon = str(data.get('weather', '')[0].get('icon', ''))[:-1]
+
+
+        return (wTime, wTemp, wIcon, wDesc)
+        #return 'Das Wetter um ' + wTime + ': ' + str(data.get('weather', '')[0].get('description', '').encode('utf-8')) + ' bei ' + str(data.get('main', '').get('temp', '')) + '°C\n\n' + getWeatherForecast()
+    except:
+        return 0
+        #return 'Momentan ist keine Wetterabfrage verfügbar. Versuch es später noch einmal.'
+
+def getWaterTemp(choice):
+    #Getting the actual temperature
+    actl = getWaterTempVal()
+
+    if actl == -1:
+        return 'Momentan steht keine Wassertemperatur zur Verfügung. Versuch es später noch einmal.'
+    else:
+        time = actl[1]
+
+        msg = 'Zwischen ' + str(time) + '-' + str(time + 1) + 'Uhr betrug die Wassertemperatur: '
+        if choice == 0:
+            return str(actl[0])
+            #return msg + str(actl[0]) + '°C'
+        elif choice == 1:
+            return msg + convertTempToMen(actl[0]) + 'cm'
+
+
+def getForecast():
+    try:
+        url = 'http://api.openweathermap.org/data/2.5/forecast?id=2949012&lang=de&units=metric&APPID=' + bottoken.weatherApiKey
+        response = urllib2.urlopen(url);
+        data = json.loads(response.read())
+        fcData = []
+        fStr = ''
+
+        for i in range(1,4):
+            fStr += 'Wettervorhersage für '
+            #fStr += time.strftime("%H:%M", time.gmtime(data.get('list', '')[0]))
+            fStr += time.strftime("%H:%M", time.gmtime(data.get('list', '')[i].get('dt', '')))
+            fStr += ': '
+            fStr += str(data.get('list', '')[i].get('weather')[0].get('description', '').encode('utf-8'))
+            fStr += ' bei '
+            fStr += str(data.get('list', '')[i].get('main').get('temp', ''))
+            fStr += '°C\n'
+            fcData.append([time.strftime("%H:%M", time.gmtime(data.get('list', '')[i].get('dt', ''))), "%.1f" % data.get('list', '')[i].get('main').get('temp', ''), str(data.get('list', '')[i].get('weather')[0].get('icon', '').encode('utf-8'))[:-1]])
+
+
+        return fcData 
+        #return fStr
+    except Exception, err:
+        print ('ERROR: %s\n' % str(err))
+        return 'Eine Wettervorhersage ist momentan nicht möglich.'
+    #except:
+    #    return 'Eine Wettervorhersage ist momentan nicht möglich.'
